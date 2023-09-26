4944d9a96eabd9a352648cf12014e0ec0159da08
tresyam <tressaadolf@gmail.com>
2023-09-25 20:29:52 -0600

    newest additions

---
+++ src/components/products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Product = ({ product, addToCart }) => {
+  return (
+    <div className="product">
+      <img src={product.image} alt={product.name} />
+      <h3>{product.name}</h3>
+      <p>${product.price.toFixed(2)}</p>
+      <button onClick={() => addToCart(product)}>Add to Cart</button>
+    </div>
+  );
+};
+
+export default Product;
